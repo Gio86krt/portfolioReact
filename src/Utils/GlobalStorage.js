00e356ac29f834c9401bb68f70976ef4480a08a5
Giovanni <lugk4l@gmail.com>
2021-03-27 08:36:36 -0400

    global storage obj

--- src/Utils/GlobalStorage.js
+++ src/Utils/GlobalStorage.js
@@ -1,8 +1,41 @@
 const projects = [
-  { name: "Daily planner", url: "https://gio86krt.github.io/DailyPlanner/" },
+  {
+    name: "Daily planner",
+    url: "https://gio86krt.github.io/DailyPlanner/",
+    description: "Plan your day with this useful and practical tool.",
+    image: "./planner.png",
+  },
   {
     name: "Fitness Tracker",
     url: "https://polar-retreat-19092.herokuapp.com/",
+    description:
+      "Stay ahead of your game, record your workouts and track your progress.",
+    image: "./fitnessTracker.png",
+  },
+  {
+    name: "Weather Dashboard",
+    url: "https://gio86krt.github.io/WeatherDashboard/",
+    description: "Search for a city and get the forecast for the next days.",
+    image: "./weather.png",
+  },
+  {
+    name: "Note Taker",
+    url: "https://notetakerapp01.herokuapp.com/",
+    description: "Useful app to take note.",
+    image: "./noteTaker.png",
+  },
+  {
+    name: "Clients Management System",
+    url: "https://islandsolutionscrm.herokuapp.com/",
+    description: "Store your clients data and organize your work.",
+    image: "./CRM.png",
+  },
+  {
+    name: "Reddit App",
+    url: "https://gio86krt.github.io/FrontEnd-EasyRedditTrackerForCrypto/",
+    description:
+      "Be constantly up to date on latest crypto currencies value with this useful App.",
+    image: "./reddit.png",
   },
 ];
 
